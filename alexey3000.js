e8c867bda18726e112bf02a28eb16e98ec85f474
cattyngmd <cvenby@gmail.com>
2023-06-08 17:30:22 +0300

    на всякий

--- alexey3000.js
+++ alexey3000.js
@@ -5,7 +5,7 @@ export class Alexey3000 {
   markov;
 
   constructor(props) {
-    const file = fs.readFileSync(props.dictionary || "dictionary.txt");
+    const file = fs.readFileSync(props.dictionary || "./dictionary.txt");
     const arr = file.toString().split('\n');
     
     this.markov = new MarkovGen({
